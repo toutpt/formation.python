f234e250b1594043f8a755ff2d4b63f54738b7ee
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-10-25 11:35:10 +0200

    add tests suite

---
+++ formation/python/tkinter/addressbook/tests/test_contact.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestContact(unittest.TestCase):
+    """contact testing"""
+    
+    def setUp(self):
+        pass
+
+    def test_name(self):
+        self.assertTrue(False, "wtf")
